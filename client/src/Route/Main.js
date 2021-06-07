5ed650f18a3819892368e45eb8371bcc5f064781
Seulgi_LEE <dl4459@naver.com>
2021-06-07 16:56:38 +0900

    progress bar추가

--- client/src/Route/Main.js
+++ client/src/Route/Main.js
@@ -1,12 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,  } from 'react';
 import Search from '../components/Search';
 import Keyword from '../components/Keyword';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const style = {
     main: {
         width: '400px',
     },
+    container: {
+        border: '1px black solid',
+        
+    }
 };
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > * + *': {
+        marginLeft: theme.spacing(2),
+      },
+    },
+  }));
 
 function Main() {
     const initialValue =[{
@@ -20,8 +35,11 @@ function Main() {
         six:''
     }];
 
+    const classes = useStyles();
+
     const [keywords, setKeywords] = useState(initialValue);
     const [form, setValues] = useState(initialValue.keyword);
+    const [completed, setCompleted] = useState(0);
 
     const callApi = async () => {
         const response = await fetch('http://localhost:5000/api/search');
@@ -32,23 +50,35 @@ function Main() {
     useEffect(() => {
         try {
             callApi().then((res) => setKeywords(res));
+            if(!completed) return 0;
+            const tick = setTimeout(()=>{
+                setCompleted({completed: completed>= 100 ? 0 : completed+1})
+            }, 20);
+            return ()=>clearTimeout(tick);
         } catch (e) {
             console.log(e);
         }
-    });
+    }, [completed]);
 
     return (
-        <div className={style.main}>
+        <div style={style.main}>
             <div>자료 조사 봇</div>
-            <Search form={form}/>
-            <div>
-                {keywords ? (
-                    <div>
-                        {keywords.map(c => {
-                        return <Keyword key={c.id} one={c.one} two={c.two} trd={c.trd} four={c.four} five={c.five} six={c.six}/>
-                        })}
-                    </div>
-                ):("")}
+            <div style={style.container}>
+                <Search form={form}/>
+                <div>
+                    {keywords ? (
+                        <div>
+                            {keywords.map(c => {
+                            return <Keyword key={c.id} one={c.one} two={c.two} trd={c.trd} four={c.four} five={c.five} six={c.six}/>
+                            })}
+                        </div>
+                    ):(
+                        <div>
+                            <CircularProgress className={classes.root} variant="determinate" value={completed}/>
+                        </div>
+                    )}
+                </div>
+                
             </div>
         </div>
     );

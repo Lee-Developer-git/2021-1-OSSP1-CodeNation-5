fc638921ece706dfc8079bad8378fec017b3f007
Seulgi_LEE <dl4459@naver.com>
2021-06-10 19:19:16 +0900

    키워드 버튼 선택기능

--- client/src/components/Keyword.js
+++ client/src/components/Keyword.js
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Form } from 'reactstrap';
+import Research from './Research';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
@@ -14,15 +15,75 @@ const style = makeStyles((theme)=>({
 function Keyword({ one, two, trd, four, five, six }) {
     const classes = style;
 
+    const initialState ={
+        btnOne: 'outlined',
+        btnTwo: 'outlined',
+        btnTrd: 'outlined',
+        btnFour: 'outlined',
+        btnFive: 'outlined',
+        btnSix: 'outlined',
+    };
+
+    const [variant, setVarient] = useState(initialState);
+
+    const changeColor = async(e) =>{
+        console.log(e.target.name);
+        console.log(e.target.variant);
+        await setVarient({
+            ...variant,
+            [e.target.name] : 'contained'
+        });
+    }
+
     return(
-        <div>
-            <Button variant="outlined" color="primary" style={classes.none_click}>{one}</Button>
-            <Button variant="outlined" color="primary" style={classes.none_click}>{two}</Button>
-            <Button variant="outlined" color="primary" style={classes.none_click}>{trd}</Button>
-            <Button variant="outlined" color="primary" style={classes.none_click}>{four}</Button>
-            <Button variant="outlined" color="primary" style={classes.none_click}>{five}</Button>
-            <Button variant="outlined" color="primary" style={classes.none_click}>{six}</Button>
-        </div>
+        <>
+            <Form >
+                <Button
+                    id="btnOne"
+                    name="btnOne"
+                    variant={variant.btnOne}
+                    color="primary"
+                    style={classes.none_click}
+                    onClick={changeColor}>{one}</Button>
+                <Button
+                    id="btnTwo"
+                    name="btnTwo"
+                    variant={variant.btnTwo}
+                    color="primary"
+                    style={classes.none_click}
+                    onClick={changeColor}>{two}</Button>
+                <Button
+                    id="btnTrd"
+                    name="btnTrd"
+                    variant={variant.btnTrd}
+                    color="primary"
+                    style={classes.none_click}
+                    onClick={changeColor}>{trd}</Button>
+                <Button
+                    id="btnFour"
+                    name="btnFour"
+                    variant={variant.btnFour}
+                    color="primary"
+                    style={classes.none_click}
+                    onClick={changeColor}>{four}</Button>
+                <Button
+                    id="btnFive"
+                    name="btnFive"
+                    variant={variant.btnFive}
+                    color="primary"
+                    style={classes.none_click}
+                    onClick={changeColor}>{five}</Button>
+                <Button
+                    id="btnSix"
+                    name="btnSix"
+                    variant={variant.btnSix}
+                    color="primary"
+                    style={classes.none_click}
+                    onClick={changeColor}>{six}</Button><br/><br/>
+                <Button type="submit" variant="contained" color="primary" style={{ width:"100%" }}>검색</Button>
+            </Form>
+            <Research />
+        </>
     );
 }
 
